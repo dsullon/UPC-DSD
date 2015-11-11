757881c57fa7f59fb654c72e682cb47089db0edc
Marcos Lear <marcos_808@hotmail.com>
2015-11-10 21:07:15 -0500

    Creacion de clase aptitud

---
+++ SOAPServices/Dominio/Aptitud.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace SOAPServices.Dominio
+{
+    [DataContract]
+    public class Empresa
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Descripcion { get; set; }
+        [DataMember]
+        public int Valor { get; set; }
+
+
+    }
+}
