17d9dd43299df2d58ae5aac97693e868e6ca00c5
Alex Sullon <alexsullonporras@hotmail.com>
2015-11-17 00:12:21 -0500

    Cambio en las validaciones de ingreso de datos

--- Reclutamiento.MVC/Controllers/EmpresaController.cs
+++ Reclutamiento.MVC/Controllers/EmpresaController.cs
@@ -13,12 +13,14 @@ namespace Reclutamiento.MVC.Controllers
         // GET: Empresa
         public ActionResult Index()
         {
+            var listaRubros = proxy.ListarRubros();
+            ViewBag.Rubros = listaRubros;
             return View();
         }
 
-        public JsonResult ExisteRUC(string numeroRUC)
+        public JsonResult ExisteRUC(string ruc)
         {
-            var empresa = proxy.ListarEmpresas().Where(c => c.NumeroRuc.Equals(numeroRUC)).FirstOrDefault();
+            var empresa = proxy.ListarEmpresas().Where(c => c.NumeroRuc.Equals(ruc)).FirstOrDefault();
             if (empresa != null)
                 return new JsonResult { Data = false };
             return new JsonResult { Data = true };
@@ -29,7 +31,43 @@ namespace Reclutamiento.MVC.Controllers
 
         }
 
-    }
+        public JsonResult ExisteEmail(string email)
+        {
+            var empresa = proxy.ListarEmpresas().Where(c => c.EmailContacto.Equals(email)).FirstOrDefault();
+            if (empresa != null)
+                return new JsonResult { Data = false };
+            return new JsonResult { Data = true };
+        }
 
+        public ActionResult Registrar()
+        {
+            var listaRubros = proxy.ListarRubros();
+            ViewBag.Rubros = listaRubros;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Empresa empresa, int rubro)
+        {
+            var r = proxy.ObtenerRubro(rubro);
+            //OperationStatus opStatus = proxy.CrearEmpresa(empresa.EmailContacto, empresa.Clave, empresa.RazonSocial, empresa.NumeroRuc, empresa.Rubro.Id);
+            OperationStatus opStatus = proxy.CrearEmpresa(empresa.EmailContacto, empresa.Clave, empresa.RazonSocial, empresa.NumeroRuc, r.Id);
+            return Json(opStatus);
+            //var result = proxy.CrearEmpresa(empresa.EmailContacto, empresa.Clave, empresa.RazonSocial, empresa.NumeroRuc, empresa.Rubro.Id);
+            //if (result.Success)
+            //    return RedirectToAction("Index", "Home");
+            //else
+            //{
+            //    ViewBag.Error = TempData["error"];
+            //    return View();
+            //}
+        }
+
+        public ActionResult Listado()
+        {
+            var resultado = proxy.ListarEmpresas();
+            return View(resultado);
+        }
+    }
 
 }
