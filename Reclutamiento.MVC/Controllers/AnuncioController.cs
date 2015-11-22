931ccf6ed5ed8f23837fe0f97e44e37060691fce
Oscar Leiva <leivaoscar2010@gmail.com>
2015-11-22 14:33:01 -0500

    Creacion de controlador Anuncio y Aptitud

---
+++ Reclutamiento.MVC/Controllers/AnuncioController.cs
@@ -0,0 +1,91 @@
+﻿using Reclutamiento.MVC.ReclutamientoWS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reclutamiento.MVC.Controllers
+{
+    public class AnuncioController : Controller
+    {
+        ReclutamientoServiceClient proxy = new ReclutamientoServiceClient();
+        // GET: Anuncio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Anuncio/Details/5
+        public ActionResult Detalles(int id)
+        {
+            return View();
+        }
+
+        // GET: Anuncio/Create
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        // POST: Anuncio/Create
+        [HttpPost]
+        public ActionResult Registrar(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Anuncio/Edit/5
+        public ActionResult Editar(int id)
+        {
+            return View();
+        }
+
+        // POST: Anuncio/Edit/5
+        [HttpPost]
+        public ActionResult Editar(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Anuncio/Delete/5
+        public ActionResult Eliminar(int id)
+        {
+            return View();
+        }
+
+        // POST: Anuncio/Delete/5
+        [HttpPost]
+        public ActionResult Eliminar(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Reclutamiento.MVC/Controllers/AptitudController.cs
@@ -0,0 +1,91 @@
+﻿using Reclutamiento.MVC.ReclutamientoWS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reclutamiento.MVC.Controllers
+{
+    public class AptitudController : Controller
+    {
+        ReclutamientoServiceClient proxy = new ReclutamientoServiceClient();
+        // GET: Aptitud
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Aptitud/Details/5
+        public ActionResult Detalles(int id)
+        {
+            return View();
+        }
+
+        // GET: Aptitud/Create
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        // POST: Aptitud/Create
+        [HttpPost]
+        public ActionResult Registrar(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Aptitud/Edit/5
+        public ActionResult Editar(int id)
+        {
+            return View();
+        }
+
+        // POST: Aptitud/Edit/5
+        [HttpPost]
+        public ActionResult Editar(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Aptitud/Delete/5
+        public ActionResult Eliminar(int id)
+        {
+            return View();
+        }
+
+        // POST: Aptitud/Delete/5
+        [HttpPost]
+        public ActionResult Eliminar(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
